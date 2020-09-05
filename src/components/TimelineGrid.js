5f9ca3a12b876cbc1a9b1da2e6085479c0d21819
began91 <began91@yahoo.com>
2020-09-05 17:39:54 -0500

    Active minute bar

--- src/components/TimelineGrid.js
+++ src/components/TimelineGrid.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import './TimelineGrid.css';
 
 const TimelineGrid = () => {
+
+    const [minute, setMinute] = useState(Math.round((moment()-moment('08','HH'))/1000/60))
+    
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setMinute(minute => minute + 1);
+            console.log(minute);
+        }, 60000);
+        return () => clearInterval(interval);
+    }, [minute])
     
     let hours = [];
-    for (let i = 0; i < 68/4; i++) {
-        const time = moment(i+8, 'HH').format('HHmm');
+    for (let i = 0; i < 17; i++) {
+        let active = null;
+        if (Math.floor(minute/60)===i) {
+            active = Math.floor((minute%60)/15);
+        }
         hours.push(
-            <div className="hour" key={time}>
-                <div className="quarter quarter1"></div>
-                <div className="quarter quarter2"></div>
-                <div className="quarter quarter3"></div>
-                <div className="quarter quarter4"></div>
+            <div className="hour" key={i}>
+                <div className={`quarter quarter1 ${active===0 && 'active-quarter'}`}></div>
+                <div className={`quarter quarter2 ${active===1 && 'active-quarter'}`}></div>
+                <div className={`quarter quarter3 ${active===2 && 'active-quarter'}`}></div>
+                <div className={`quarter quarter4 ${active===3 && 'active-quarter'}`}></div>
             </div>
         )
     }
     
     return (
         <div id="schedule-grid">
-            {hours}            
+            {hours}
+            {minute < 1020 && <div id="minute" style={{gridColumnStart: minute}}>&nbsp;</div>}
         </div>
     )
 }
