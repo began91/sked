7acd9a5a7b9b757ff13e5c8750f5727015c5b19a
began91 <began91@yahoo.com>
2020-09-13 21:41:04 -0500

    Add Custom time hook

---
+++ src/hooks/useTime.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+
+const useTime = () => {
+    let refreshCycle = 1000*30;
+    //returns current time and re-renders every 'refresh cycle' ms
+    // inspired by: https://medium.com/javascript-in-plain-english/usetime-react-hook-f57979338de
+
+    const [time, setTime] = useState(moment().format('HHmm'));
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setTime(moment().format('HHmm'));
+        }, refreshCycle);
+        return ()=> clearInterval(interval);
+    }, [refreshCycle, moment, setInterval, clearInterval, setTime]);
+
+    return time; // returns time string in format HHmm eg 0800, 2359, etc
+}
+
+export default useTime;
