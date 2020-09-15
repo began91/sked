d073f8dbc7fcd1882b71b430a0de732ea7f28639
began91 <began91@yahoo.com>
2020-09-14 22:46:17 -0500

    Updates

---
+++ precache-manifest.138388d5e050a669bca32ff0e8fcb2b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544edf6e976441795fd0cd205ba82b9f",
+    "url": "/sked/index.html"
+  },
+  {
+    "revision": "60ebda34676e58d08a51",
+    "url": "/sked/static/css/main.a218ada4.chunk.css"
+  },
+  {
+    "revision": "e9777ce9e40ed4c23eb3",
+    "url": "/sked/static/js/2.0f9f34b0.chunk.js"
+  },
+  {
+    "revision": "865f5a305cd05b6ff520ea4ee8ad6f58",
+    "url": "/sked/static/js/2.0f9f34b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ebda34676e58d08a51",
+    "url": "/sked/static/js/main.11ef9c34.chunk.js"
+  },
+  {
+    "revision": "49cddb7fe394313b5177",
+    "url": "/sked/static/js/runtime-main.279d0e74.js"
+  },
+  {
+    "revision": "0bbd909b5588f009cbbadf9bc85595d5",
+    "url": "/sked/static/media/28patch.0bbd909b.png"
+  }
+]);
